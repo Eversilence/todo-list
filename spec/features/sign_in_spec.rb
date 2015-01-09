b0ef888d57c78c20176615739f3b8b7aee06456a
Eversilence <denis.kostyrko.apps@gmail.com>
2015-01-09 19:18:16 +0200

    Removed hard-coded credentials inside sign_in test

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -2,8 +2,8 @@ require "rails_helper"
 require "spec_helper"
 
 describe "sign in process", :js => true do
-  let(:email)    { Faker::Internet.email}
-  let(:password) { Faker::Internet.password(8) }
+  let!(:email)    { Faker::Internet.email}
+  let!(:password) { Faker::Internet.password(8) }
 
   it "signs me up" do
     visit '/users/sign_up'
@@ -16,9 +16,9 @@ describe "sign in process", :js => true do
 
   it "signs me in" do
     visit '/users/sign_in'
-    create(:user, email: 'irdis.evale@gmail.com', password: '7214661e')
-    fill_in 'Email', :with => 'irdis.evale@gmail.com'
-    fill_in 'Password', :with => '7214661e'
+    create(:user, email: email, password: password)
+    fill_in 'Email', :with => email
+    fill_in 'Password', :with => password
     click_button 'Sign in'
     expect(page).to have_content 'Signed in successfully'
   end
